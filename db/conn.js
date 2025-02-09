b8d6318c6d78acc4d755b157bcd610bae8f7ae41
TanviPansuriya <tanvi.coderkube@gmail.com>
2025-02-09 16:15:10 +0530

    09-02-2025 4:15

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Cricket_Box_Booking").then(() =>
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URL).then(() =>
     console.log("Connection successfully...")
 ).catch((err) =>
     console.log("Eroor..." + err)
