a0f7a4f55850a13e33f656b2bd967c89cf4f84c6
TanviPansuriya <tanvi.coderkube@gmail.com>
2025-02-21 09:07:00 +0530

    21-02-25

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -20,48 +20,4 @@ router.get('/totalTurfs', auth, totalTurfs);
 router.get('/totalBookings', auth, totalBookings);
 router.get('/getAllTimeSlots/:turfId', auth, getAllTimeSlots);
 
-
-
 module.exports = router;
-
-
-
-
-
-// const express = require("express");
-// const {
-//   getAllTurfs,
-//   getTurfById,
-//   createTurf,
-//   updateTurf,
-//   deleteTurf,
-// } = require("../controllers/turfController");
-
-// const router = express.Router();
-
-// router.get("/", getAllTurfs);        // Get all turfs
-// router.get("/:id", getTurfById);      // Get a single turf by ID
-// router.post("/", createTurf);         // Create a new turf
-// router.put("/:id", updateTurf);       // Update a turf
-// router.delete("/:id", deleteTurf);    // Delete a turf
-
-// // module.exports = router;
-
-
-// {
-//      "name": "Green Field Turf",
-//      "location": { "lat": 19.076, "lng": 72.8777 },
-//      "googleMapLink": "https://goo.gl/maps/example",
-//      "address1": "123 Sports Complex",
-//      "address2": "Near City Park",
-//      "city": "Mumbai",
-//      "landmark": "Opposite Metro Station",
-//      "zipcode": "400001",
-//      "images": ["https://example.com/turf1.jpg", "https://example.com/turf2.jpg"],
-//      "contact": { "phone": "+91 9876543210", "email": "contact@turf.com" },
-//      "timeSlots": [
-//        { "startTime": "06:00", "endTime": "08:00", "price": 500 },
-//        { "startTime": "08:00", "endTime": "10:00", "price": 700 }
-//      ]
-//    }
-   
